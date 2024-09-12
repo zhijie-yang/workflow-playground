418695345ca3bd26c2a8dac5c69c32761b0a9ede
Zhijie Yang <zhijie.yang@canonical.com>
2024-09-12 11:47:00 +0200

    should cancel

--- .github/actions/validate-access/validate-access.sh
+++ .github/actions/validate-access/validate-access.sh
@@ -8,25 +8,29 @@ image_path=$4
 echo "github.actor: ${actor}"
 echo "admin-only: ${admin_only}"
 if [[ ${admin_only} == true ]]; then
+    exit_status=0
     echo "Expanding team mentions in the CODEOWNERS file"
+    cp ${workspace}/CODEOWNERS ${workspace}/CODEOWNERS.bak
     teams=$(grep -oE '@[[:alnum:]_.-]+\/[[:alnum:]_.-]+' ${workspace}/CODEOWNERS || true | sort | uniq)
 
-    for team in $teams; do
-    org=$(echo $team | cut -d'/' -f1 | sed 's/@//')
-    team_name=$(echo $team | cut -d'/' -f2)
-    members=$(gh api "/orgs/$org/teams/$team_name/members" | jq -r '.[].login')
-    replacement=$(echo "$members" | xargs -I {} echo -n "@{} " | awk '{$1=$1};1')
-    sed -i "s|$team|$replacement|g" ${workspace}/CODEOWNERS
+    for team in ${teams}; do
+        org=$(echo ${team} | cut -d'/' -f1 | sed 's/@//')
+        team_name=$(echo ${team} | cut -d'/' -f2)
+        members=$(gh api "/orgs/${org}/teams/${team_name}/members" | jq -r '.[].login')
+        replacement=$(echo "${members}" | xargs -I {} echo -n "@{} " | awk '{$1=$1};1')
+        sed -i "s|${team}|${replacement}|g" ${workspace}/CODEOWNERS
     done
 
     if grep -wq "@${actor}" ${workspace}/CODEOWNERS; then
-    echo "The workflow is triggered by ${actor} as the code owner"
-    elif cat ${workspace}/${{ inputs.image-path }}/contacts.yaml | yq ".maintainers" | grep -wq "${actor}"; then
-    echo "The workflow is triggered by ${actor} as a maintainer of the image ${{ inputs.image-path }}"
+        echo "The workflow is triggered by ${actor} as the code owner"
+    elif cat ${workspace}/${image_path}/contacts.yaml | yq ".maintainers" | grep "\- " | grep -wq "${actor}"; then
+        echo "The workflow is triggered by ${actor} as a maintainer of the image ${image_path}"
     else
-    echo "The workflow is triggered by a user neither as a code owner nor a maintainer of the image ${{ inputs.image-path }}"
-    exit 1
+        echo "The workflow is triggered by a user neither as a code owner nor a maintainer of the image ${image_path}"
+        exit_status=1
     fi
+    mv ${workspace}/CODEOWNERS.bak ${workspace}/CODEOWNERS
+    exit ${exit_status}
 else
     echo "The workflow is not restricted to non-code-owner or non-maintainer users"
 fi
